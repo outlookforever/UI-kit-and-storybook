bf4f20aa375b8c835547832152f9de28035b4f6a
Sergey Kochetov <anashkagonit@gmail.com>
2024-04-24 21:15:52 +0300

    Storybook ~ first button components with variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,52 @@
 import { ComponentProps } from 'react'
+import { VariantProps, cva } from 'class-variance-authority'
+import { cn } from '@/utils'
 
-type ButtonProps = ComponentProps<'button'>
+const buttonStyles = cva(
+	['w-full', 'rounded-md', 'font-semibold', 'focus:outline-none', 'disabled:cursor-not-allowed'],
+	{
+		variants: {
+			variant: {
+				solid: '',
+				outline: 'border-2',
+				ghost: 'transition-colors duration-300'
+			},
+			size: {
+				sm: 'px-4 py-2 text-sm',
+				md: 'px-4 py-2 text-base',
+				lg: 'px-6 py-3 text-lg'
+			},
+			colorschema: {
+				primary: 'text-white'
+			}
+		},
+		compoundVariants: [
+			{
+				variant: 'solid',
+				colorschema: 'primary',
+				className: 'bg-primary-500 hover:bg-primary-600'
+			},
+			{
+				variant: 'outline',
+				colorschema: 'primary',
+				className: 'text-primary-600 border-primary-500 bg-transparent hover:bg-primary-100'
+			},
+			{
+				variant: 'ghost',
+				colorschema: 'primary',
+				className: 'text-primary-600 bg-transparent hover:bg-primary-100'
+			}
+		],
+		defaultVariants: {
+			variant: 'solid',
+			size: 'md',
+			colorschema: 'primary'
+		}
+	}
+)
 
-export const Button = ({ ...props }: ButtonProps) => {
-	return <button className="text-blue-500" {...props} />
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof buttonStyles>
+
+export const Button = ({ variant, size, colorschema, className, ...props }: ButtonProps) => {
+	return <button className={cn(buttonStyles({ variant, size, colorschema, className }))} {...props} />
 }
