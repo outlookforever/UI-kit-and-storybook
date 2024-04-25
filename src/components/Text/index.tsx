3d72272aca26044834779c5ced5c613b59da0892
Sergey Kochetov <anashkagonit@gmail.com>
2024-04-25 20:09:14 +0300

    Text ~ add styles for component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,6 +1,46 @@
+import { VariantProps, cva } from 'class-variance-authority'
 import { ComponentProps } from 'react'
 
-type TextProps = ComponentProps<'span'>
+const textStyles = cva('w-full', {
+	variants: {
+		emphasis: {
+			low: 'text-gray-600 font-light'
+		},
+		size: {
+			sm: 'text-sm',
+			base: 'text-base',
+			lg: 'text-lg',
+			xl: 'text-xl',
+			'2xl': 'text-2xl',
+			'3xl': 'text-3xl'
+		},
+		weight: {
+			thin: 'font-thin',
+			normal: 'font-normal',
+			medium: 'font-medium',
+			semibold: 'font-semibold',
+			bold: 'font-bold',
+			black: 'font-black'
+		},
+		align: {
+			left: 'text-left',
+			right: 'text-right',
+			center: 'text-center'
+		},
+		italic: {
+			true: 'italic'
+		},
+		underline: {
+			true: 'underline underline-offset-2'
+		}
+	},
+	defaultVariants: {
+		size: 'base',
+		align: 'left'
+	}
+})
+
+type TextProps = ComponentProps<'span'> & VariantProps<typeof textStyles>
 
 export const Text = ({ ...props }: TextProps) => {
 	return <span {...props}>Text</span>
